1f544285ba2b127e463351b0158b191aabee1a51
TitusWongCN <wzhwno1@163.com>
2020-11-24 21:00:57 +0800

    Index page done

---
+++ pages/category/category.js
@@ -0,0 +1,37 @@
+import { request } from "../../request/index.js"
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    leftMenuList: [],
+    rightContent: [],
+  },
+
+  Cates: [],
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getCates();
+  },
+
+  getCates(){
+    request({
+      url:"https://api-hmugo-web.itheima.net/api/public/v1/categories"
+    })
+    .then(result=>{
+      this.Cates=result.data.message;
+
+      let leftMenuList = this.Cates.map(v=>v.cat_name);
+      let rightContent = this.Cates[0].children;
+      this.setData({
+        leftMenuList,
+        rightContent
+      })
+    })
+  }
+})
